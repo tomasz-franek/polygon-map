c90f9338c209a912a15ad3f8aafd5fc9c0382683
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-04-02 15:29:17 +0200

    added repository annotation

--- backend/src/main/java/org/polygonMap/backend/repositories/MapSlideShowRepository.java
+++ backend/src/main/java/org/polygonMap/backend/repositories/MapSlideShowRepository.java
@@ -2,7 +2,9 @@ package org.polygonMap.backend.repositories;
 
 import org.polygonMap.model.MapSlideShow;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MapSlideShowRepository extends MongoRepository<MapSlideShow, String> {
     MapSlideShow findByMapSlideShowId(String mapSlideShowId);
 }
--- backend/src/main/java/org/polygonMap/backend/repositories/PolygonRepository.java
+++ backend/src/main/java/org/polygonMap/backend/repositories/PolygonRepository.java
@@ -2,7 +2,9 @@ package org.polygonMap.backend.repositories;
 
 import org.polygonMap.model.Polygon;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PolygonRepository extends MongoRepository<Polygon, String> {
     Polygon findByPolygonId(String polygonId);
 }
