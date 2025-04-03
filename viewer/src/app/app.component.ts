a3b39992a57b34732faa7cb359d60880f158aa59
tomasz <23628164+tomasz-franek@users.noreply.github.com>
2025-04-03 19:36:42 +0200

    using Slide object on map

--- viewer/src/app/app.component.ts
+++ viewer/src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {LeafletControlLayersConfig, LeafletModule} from '@bluehalo/ngx-leaflet';
 import {LatLng, latLng, polygon, tileLayer} from 'leaflet';
 import * as jsonData from '../assets/europe.json';
+import {v4 as uuid} from 'uuid';
+import {Slide} from './api/slide';
 
 @Component({
   selector: 'app-root',
@@ -20,37 +22,27 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    jsonData.features.forEach((feature:any) => {
+    const slide: Slide = this.parseJsonData();
+    slide.polygons.forEach(polygonObject => {
+      const points: LatLng[] = [];
+      if (polygonObject.coordinates[0].length == 2) {
+        polygonObject.coordinates.forEach((item: number[]) => {
+          if (typeof item[0] == 'number' && typeof item[1] == 'number') {
+            points.push(new LatLng(item[0], item[1]));
+          }
+        })
+        this.layersControl.overlays[polygonObject.polygonId || ''] = polygon(points);
+      } else {
 
-      if (feature.geometry.type == 'Polygon') {
-        const points: LatLng[] = [];
-        feature.geometry.coordinates.forEach((coordinate:any) => {
-          coordinate.forEach((item:any) => {
-            if(typeof item[0] == 'number' && typeof item[1] == 'number') {
-              points.push(new LatLng(item[1],item[0]));
-            }
-          })
-        })
-        this.layersControl.overlays[feature.properties.NAME] =  polygon(points);
-      }
-      if (feature.geometry.type == 'MultiPolygon') {
         const polyLinePoints: any = [];
-        feature.geometry.coordinates.forEach((coordinate:any) => {
-          coordinate.forEach((item:any) => {
-            const points: LatLng[] = [];
-            if(item instanceof Array) {
-                item.forEach((multiItem:any) => {
-                  if (multiItem instanceof Array) {
-                    if (typeof multiItem[0] == 'number' && typeof multiItem[1] == 'number') {
-                      points.push(new LatLng(multiItem[1], multiItem[0]));
-                    }
-                  }
-                })
-              }
-            polyLinePoints.push(points);
+        polygonObject.coordinates.forEach((coordinate: any[]) => {
+          const points: LatLng[] = [];
+          coordinate.forEach((item: any[]) => {
+            points.push(new LatLng(item[0], item[1]));
           })
+          polyLinePoints.push(points);
         })
-        this.layersControl.overlays[feature.properties.NAME] =  polygon(polyLinePoints);
+        this.layersControl.overlays[polygonObject.polygonId || ''] = polygon(polyLinePoints);
       }
     })
   }
@@ -60,7 +52,45 @@ export class AppComponent implements OnInit {
       tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 15, attribution: 'OpenStreetMap' })
     ],
     zoom: 5,
-    center: latLng(45.801919,19.415062)
+    center: latLng(51.801919, 19.415062)
   };
 
+  parseJsonData(): Slide {
+    let slide: Slide = {
+      slideId: uuid(),
+      polygons: []
+    };
+    jsonData.features.forEach((feature: any) => {
+
+      if (feature.geometry.type == 'Polygon') {
+        const points: Array<Array<number>> = [];
+        feature.geometry.coordinates.forEach((coordinate: any) => {
+          coordinate.forEach((item: any) => {
+            points.push([item[1], item[0]]);
+          })
+        })
+        slide.polygons.push({coordinates: points, polygonId: feature.properties.NAME, id: uuid()});
+      }
+      if (feature.geometry.type == 'MultiPolygon') {
+        const polyLinePoints: any = [];
+        feature.geometry.coordinates.forEach((coordinate: any) => {
+          coordinate.forEach((item: any) => {
+            const points: Array<Array<number>> = [];
+            if (item instanceof Array) {
+              item.forEach((multiItem: any) => {
+                if (multiItem instanceof Array) {
+                  if (typeof multiItem[0] == 'number' && typeof multiItem[1] == 'number') {
+                    points.push([multiItem[1], multiItem[0]]);
+                  }
+                }
+              })
+            }
+            polyLinePoints.push(points);
+          })
+        })
+        slide.polygons.push({coordinates: polyLinePoints, polygonId: feature.properties.NAME, id: uuid()});
+      }
+    })
+    return slide;
+  }
 }
